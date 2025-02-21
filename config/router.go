f8b765fb8d6c824d3e913f18dccf0aa09a47070b
Phạm Ngọc Trường Sơn <sonpnts@ou.edu.vn>
2025-02-21 21:44:27 +0700

    fix CROSS

--- config/router.go
+++ config/router.go
@@ -1,14 +1,14 @@
 package config
 
 import (
-	"github.com/sonpnts/todo-list/handlers"
-
 	"github.com/gin-gonic/gin"
+	"github.com/sonpnts/todo-list/handlers"
+	"github.com/sonpnts/todo-list/middleware"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-
+	r.Use(middleware.CORSConfig())
 	r.POST("/tasks", handlers.CreateTask)
 	r.GET("/tasks", handlers.GetTasks)
 	r.PUT("/tasks/:id", handlers.UpdateTask)
---
+++ middleware/cors.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func CORSConfig() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
